363dab9c8e7e583b22116e59d1cd2ecaee3d53da
anhtuanMDev <anhtuan03.mdev@gmail.com>
2026-04-21 15:20:14 +0700

    chore: configure production deployment with Procfile and environment-based settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tribe_backend.wsgi --log-file -
--- tribe_backend/settings.py
+++ tribe_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%_a_l4$rqv8@mv5!1f-p5aces*7$dw1#!)9#g#f1_e$e^vnuim"
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-for-local-dev-only")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -142,6 +143,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "tribeapp.official@gmail.com"
-EMAIL_HOST_PASSWORD = "fges cabb gqbh bgbh"
-DEFAULT_FROM_EMAIL = "Tribe <tribeapp.official@gmail.com>"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = f"Tribe <{os.environ.get('EMAIL_HOST_USER')}>"
