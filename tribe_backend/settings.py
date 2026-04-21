018b7ce1459db07eb6a2bb589e9fa41035250e47
anhtuanMDev <anhtuan03.mdev@gmail.com>
2026-04-21 15:39:55 +0700

    feat: configure default console logging in settings

--- tribe_backend/settings.py
+++ tribe_backend/settings.py
@@ -141,3 +141,18 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = f"Tribe <{os.environ.get('EMAIL_HOST_USER')}>"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
